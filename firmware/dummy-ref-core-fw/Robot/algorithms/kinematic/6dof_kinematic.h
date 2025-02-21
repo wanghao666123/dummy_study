36d89359c57923999f646698b3c756f163df01da
wh <572737004@qq.com>
2025-02-21 18:19:43 +0800

    end learn 6df by wanghao 2025.2.21

--- firmware/dummy-ref-core-fw/Robot/algorithms/kinematic/6dof_kinematic.h
+++ firmware/dummy-ref-core-fw/Robot/algorithms/kinematic/6dof_kinematic.h
@@ -8,26 +8,41 @@
 class DOF6Kinematic
 {
 private:
+    //!将 弧度 转换为 度
     const float RAD_TO_DEG = 57.295777754771045f;
 
     // DH parameters
     struct ArmConfig_t
     {
-        float L_BASE;
-        float D_BASE;
-        float L_ARM;
-        float L_FOREARM;
-        float D_ELBOW;
-        float L_WRIST;
+        float L_BASE;//!机器人的基座长度
+        float D_BASE;//!基座的偏移量
+        float L_ARM;//!机器臂的长度
+        float L_FOREARM;//!前臂的长度
+        float D_ELBOW;//!肘部的偏移量
+        float L_WRIST;//!手腕的长度
     };
     ArmConfig_t armConfig;
+    //!DH_matrix 是一个 6x4 的矩阵，用于存储机器人的 DH 参数，即 Denavit-Hartenberg 参数。每一行代表一个关节的四个参数：
+
+    //!home：该关节的零位（参考位）。
+    //!d：该关节的偏移量。
+    //!a：该关节的连杆长度。
+    //!alpha：该关节的扭转角度。
 
     float DH_matrix[6][4] = {0}; // home,d,a,alpha
+    //!机器人不同部件的 3D 坐标（x, y, z）。每个数组表示一个坐标位置
+    //!L1_base：基座的位置
+    //!L2_arm：臂部的末端位置
+    //!L3_elbow：肘部的位置
+    //!L6_wrist：手腕的位置
     float L1_base[3] = {0};
     float L2_arm[3] = {0};
     float L3_elbow[3] = {0};
     float L6_wrist[3] = {0};
 
+    //!l_se_2 和 l_se：可能是某个连杆的平方和长度，用于描述臂长
+    //!l_ew_2 和 l_ew：与肘部相关的长度计算
+    //!atan_e：计算肘部角度
     float l_se_2;
     float l_se;
     float l_ew_2;
@@ -53,7 +68,9 @@ public:
     {
         Pose6D_t()
         = default;
-
+        //!位置（X, Y, Z）和旋转（A, B, C）
+        //!X, Y, Z: 位置坐标，表示机器人或物体在三维空间中的位置
+        //!A, B, C: 旋转角度，可能表示物体绕 XYZ 轴的旋转角度（通常是欧拉角）
         Pose6D_t(float x, float y, float z, float a, float b, float c)
             : X(x), Y(y), Z(z), A(a), B(b), C(c), hasR(false)
         {}
@@ -64,6 +81,8 @@ public:
 
         // if Pose was calculated by FK then it's true automatically (so that no need to do extra calc),
         // otherwise if manually set params then it should be set to false.
+        //!FK（Forward Kinematics） 是指通过已知的关节角度计算机器人末端的位置和方向。如果 Pose 是通过 FK 计算得出的，那么它的旋转信息是自动计算的，不需要额外赋值，因此 hasR = true。
+        //!手动设定参数（例如直接给定目标位置和方向）时，hasR 应该是 false，表明程序需要手动处理旋转信息。
         bool hasR{};
     };
 
